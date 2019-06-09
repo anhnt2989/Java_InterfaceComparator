6acfa444c2b10c6532dfb1166d3a57cedcd80dba
anhnt2989 <anhnt2989@gmail.com>
2019-06-09 20:45:55 +0700

    nop bai

---
+++ src/main/java/CircleComparator.java
@@ -0,0 +1,9 @@
+import javafx.scene.shape.Circle;
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        return Double.compare(c1.getRadius(), c2.getRadius());
+    }
+}
